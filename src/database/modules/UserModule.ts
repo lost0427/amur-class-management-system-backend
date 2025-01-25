32a5c24626d2e3085867e2bc0a8748fd4b5176c2
Neboer <rubinposter@gmail.com>
2025-01-25 23:43:02 +0800

    修改UserModule中错误的数据库语句。

--- src/database/modules/UserModule.ts
+++ src/database/modules/UserModule.ts
@@ -19,7 +19,7 @@ export default class UserModule extends BasicDBModule {
     public async get_student_by_phone(phone_number: string): Promise<Student | null> {
         const sql = `SELECT *
                      FROM student
-                     WHERE phone_number = ?`
+                     WHERE phone_number = $1`
         const params = [phone_number]
         return await this.query_one<Student>(sql, params)
     }
@@ -27,7 +27,7 @@ export default class UserModule extends BasicDBModule {
     public async get_student_by_id(id: number): Promise<Student | null> {
         const sql = `SELECT *
                      FROM student
-                     WHERE id = ?`
+                     WHERE id = $1`
         const params = [id]
         return await this.query_one<Student>(sql, params)
     }
@@ -35,7 +35,7 @@ export default class UserModule extends BasicDBModule {
     public async get_admin_by_name(username: string): Promise<Admin | null> {
         const sql = `SELECT *
                      FROM admin
-                     WHERE username = ?`
+                     WHERE username = $1`
         const params = [username]
         return await this.query_one<Admin>(sql, params)
     }
@@ -48,19 +48,19 @@ export default class UserModule extends BasicDBModule {
 
     public async update_student(student: Student): Promise<void> {
         const sql = `UPDATE student
-                     SET name         = ?,
-                         phone_number = ?,
-                         password     = ?
-                     WHERE id = ?`
+                     SET name         = $1,
+                         phone_number = $2,
+                         password     = $3
+                     WHERE id = $4`
         const params = [student.name, student.phone_number, student.password, student.id]
         await this.query(sql, params)
     }
 
     public async update_admin(admin: Admin): Promise<void> {
         const sql = `UPDATE admin
-                     SET username = ?,
-                         password = ?
-                     WHERE id = ?`
+                     SET username = $1,
+                         password = $2
+                     WHERE id = $3`
         const params = [admin.username, admin.password, admin.id]
         await this.query(sql, params)
     }
@@ -68,7 +68,7 @@ export default class UserModule extends BasicDBModule {
     public async delete_student(id: number): Promise<void> {
         const sql = `DELETE
                      FROM student
-                     WHERE id = ?`
+                     WHERE id = $1`
         const params = [id]
         await this.query(sql, params)
     }
